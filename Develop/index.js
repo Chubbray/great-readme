eb3277eb0699ccb1f218a13855720bd9460effda
Chubbray <chubbray1@gmail.com>
2020-06-28 18:30:58 -0600

    instaled inquirer and put in one question

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,42 @@
 // array of questions for user
-const questions = [
+const fs = require('fs');
+const questions = require('inquirer');
 
-];
+questions
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project'
+        },
+    ])
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
 
-// function to initialize program
-function init() {
 
-}
+    // function to write README file
+    // function writeToFile(fileName, data) {
+    // }
+    .then(function (data) {
+        const filename =
+            data.name
+                .toLowerCase()
+                .split(' ')
+                .join('') + '.json';
 
-// function call to initialize program
-init();
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log('Success!');
+        });
+    });
+
+// // function to initialize program
+// function init() {
+
+// }
+
+
+// // function call to initialize program
+// init();
