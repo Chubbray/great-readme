f4163732552d98ac0d3b335b7509785623e48bf6
Chubbray <chubbray1@gmail.com>
2020-07-02 15:59:15 -0600

    added js and ran questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,36 +1,78 @@
 // array of questions for user
 const fs = require('fs');
-const questions = require('inquirer');
+const inquirer = require('inquirer');
 
-questions
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project'
-        },
-    ])
+const questions = [
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github User Name'
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your e-mail address'
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project'
+    },
+
+    {
+        type: 'input',
+        name: 'discription',
+        message: 'Please wright a short discription of your project'
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license would you like',
+        choices: [
+            'MIT',
+            'Apache 2.0',
+            'GPL 3.0',
+            'BSD 3',
+            'None'
+        ]
+    },
+
+    {
+        type: 'input',
+        name: 'contributers',
+        message: 'Who else has contributed to this project'
+    },
+]
 
 
 
-    // function to write README file
-    // function writeToFile(fileName, data) {
-    // }
-    .then(function (data) {
-        const filename =
-            data.name
-                .toLowerCase()
-                .split(' ')
-                .join('') + '.json';
+// function to write README file
+function writeToFile(fileName, data) {
+    return
+}
 
-        fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
-            if (err) {
-                return console.log(err);
-            }
+function init() {
+    inquirer.prompt(questions)
+        .then(function (data) {
+            const filename =
+                data.name
+                    .toLowerCase()
+                    .split(' ')
+                    .join('') + '.json';
+            console.log(data)
+            fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
+                if (err) {
+                    return console.log(err);
+                }
 
-            console.log('Success!');
+                console.log('Success!');
+            });
         });
-    });
+}
 
 // // function to initialize program
 // function init() {
@@ -39,4 +81,4 @@ questions
 
 
 // // function call to initialize program
-// init();
+ init();
